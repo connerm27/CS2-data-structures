4c883e6ceada3f268702e033cc9deeebc6931c4b
cmontg17 <cmontg17@84f5b87a-95ed-4198-8d2d-999ff41efc4e>
2020-09-11 03:44:41 +0000

    updated char constructor

--- bigint/bigint.hpp
+++ bigint/bigint.hpp
@@ -9,13 +9,15 @@
 class bigint {
 
 
+  public:
+
 	bigint(); //Default Constructor
 
 	bigint(int val); //Constructor to initilize a bigint  to a value I provide
 
-	bigint(const char[] bigVal); //Constructor to initilize a bigint to a big int val
+	bigint(const char bigVal[]);  //Constructor to initilize a bigint to a big int val
 
-	
+
 
 
 
