866a2fbe4fa022d9bd1099e72ce25789a7309bdf
cmontg17 <cmontg17@84f5b87a-95ed-4198-8d2d-999ff41efc4e>
2020-09-11 03:21:06 +0000

    updates

---
+++ cmontg17/bigint/bigint.hpp
@@ -0,0 +1,26 @@
+
+#ifndef _BIGINT_HPP_
+#define _BIGINT_HPP_
+
+
+
+#include <iostream>
+
+class bigint {
+
+
+	bigint(); //Default Constructor
+	
+	bigint(int val); //Constructor to initilize a bigint  to a value I provide
+
+	bigint(const char[] bigVal); //Constructor to initilize a bigint to a big int val
+	
+	;
+
+
+
+
+};
+
+
+#endif
