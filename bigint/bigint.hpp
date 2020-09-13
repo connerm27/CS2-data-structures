12134b912c9d260cd3e104d7e43f55042551cfca
cmontg17 <cmontg17@84f5b87a-95ed-4198-8d2d-999ff41efc4e>
2020-09-13 19:45:58 +0000

    header updates

--- bigint/bigint.hpp
+++ bigint/bigint.hpp
@@ -16,6 +16,8 @@ class bigint {
 
         bigint(const char size[]);  //Constructor to initilize a bigint
 
+	void debugPrint(std::ostream&) const;
+
 	friend std::ostream operator<<(std::ostream& out, const bigint& bi); //Overloaded <<
 
 	friend bool operator==(const bigint& bi1, const bigint& bi2); //Overloaded ==
