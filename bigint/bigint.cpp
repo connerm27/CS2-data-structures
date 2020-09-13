d15dc1aba60b981366c4da6067f8241c3e8fc91a
cmontg17 <cmontg17@84f5b87a-95ed-4198-8d2d-999ff41efc4e>
2020-09-13 23:00:31 +0000

    after tutor changes

--- bigint/bigint.cpp
+++ bigint/bigint.cpp
@@ -3,8 +3,53 @@
 
 
 bigint::bigint() {
+
+	for(int i=0; i<CAPACITY;++i) {
+		arr[i] = 0;
+	}
+}
+
+bigint::bigint(int val) {
+
+	int placeholder = val;
+
+	for(int i=0; i<CAPACITY; ++i) {
+		arr[i] = placeholder % 10;
+		placeholder = placeholder/10;
+
+		if(placeholder == 0) {
+
+			break;
+		}
+	}
+
+}
+
+bigint::bigint(const char[]) {
+
+}
+
+void bigint::debugPrint(std::ostream& out) const {
+	for(int i=CAPACITY-1; i>=0; i--) {
+		out << arr[i] << "|";
+	}
+
+}
+
+
+std::ostream operator<<(std::ostream& out, const bigint& bi) {
+
+	out << 
+
+
+
+
+}
+
+bool operator==(const bigint& bi1) const {
+
 	
-	arr[0] = 0;
+
 
 }
 
