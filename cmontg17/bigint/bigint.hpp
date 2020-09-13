7742c6b88f81355c66d1d253b6167b2c385c6207
cmontg17 <cmontg17@84f5b87a-95ed-4198-8d2d-999ff41efc4e>
2020-09-13 23:00:31 +0000

    after tutor changes

--- cmontg17/bigint/bigint.hpp
+++ cmontg17/bigint/bigint.hpp
@@ -16,11 +16,12 @@ class bigint {
 
         bigint(const char[]);  //Constructor to initilize a bigint
 
+
 	void debugPrint(std::ostream&) const;
 
-	friend std::ostream operator<<(std::ostream& out, const bigint& bi); //Overloaded <<
+	friend std::ostream operator<<(std::ostream& out, const bigint& bi); //Operator
 
-	friend bool operator==(const bigint& bi1, const bigint& bi2); //Overloaded ==
+	 bool operator==(const bigint& bi1);  //Operator
 
 
   private:
--- cmontg17/bigint/test_default_ctor.cpp
+++ cmontg17/bigint/test_default_ctor.cpp
@@ -11,7 +11,7 @@
 int main () {
     // Setup
 
-    // Test 
+    // Test
     bigint bi;
 
     // Verify
