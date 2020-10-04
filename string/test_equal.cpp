ea3df4193c0b3951c68bbe87b5a64e94817d1628
cmontg17 <cmontg17@84f5b87a-95ed-4198-8d2d-999ff41efc4e>
2020-10-04 20:28:13 +0000

    test equal tests passed

--- string/test_equal.cpp
+++ string/test_equal.cpp
@@ -7,7 +7,45 @@
 
 int main() {
 
+{
+String str(' ');
+String str2(' ');
 
+std::cout << str << " == " << str2 << std::endl;
+
+assert(str == str2);
+
+}
+
+{
+String str('a');
+String str2('a');
+
+std::cout << str << " == " << str2 << std::endl;
+
+assert(str == str2);
+
+}
+
+{
+String str("HelloWorld");
+String str2("HelloWorld");
+
+std::cout << str << " == " << str2 << std::endl;
+
+assert(str == str2);
+
+}
+
+{
+String str("test");
+String str2("test");
+
+std::cout << str << " == " << str2 << std::endl;
+
+assert(str == str2);
+
+}
 
 
 
