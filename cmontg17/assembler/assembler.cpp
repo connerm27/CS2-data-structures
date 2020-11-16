6192acb8420b26faa481c46aedafd14f1ecb7e5a
cmontg17 <cmontg17@84f5b87a-95ed-4198-8d2d-999ff41efc4e>
2020-11-16 02:23:24 +0000

    Project 3: Milestone 3, Completed

--- cmontg17/assembler/assembler.cpp
+++ cmontg17/assembler/assembler.cpp
@@ -26,32 +26,44 @@ int main(int argc, char *argv[]) {
 std::vector<String> v1;
 String s;
 while(in>>s) {  if(argc == 2) {
-		std::cout << "Must add an output file!" << std::endl;
-	break;
-	 // if(in.eof()) {
-                 //       break;
-               // }
 
 
-               // if(s != ";") {
-                 //       v1.push_back(s);
-                //      std::cout << "Made it in end of line if" << std::endl;
-               // } else {
-                //      std::cout << "made it to else" << std::endl;
-                  //      String semi(";");
-                //        v1.push_back(semi);
-              //          std::cout << "Infix: ";
-            //            for(int i=0; i < v1.size(); ++i) {
-          //                      std::cout << v1.at(i) << " ";
-        //                }
-      //                  std::cout << "\n";
 
-    //                    String rlt = infix_to_postfix(v1);
-  //                      std::cout << "Postfix: " <<  rlt << std::endl << std::endl;
-		///	String ass = evaluate(rlt);
-		//	std::cout << ass << std::endl;
-//		        v1.clear();
-              //  }
+
+
+                 if(in.eof()) {
+                        break;
+                }
+
+
+                if(s != ";") {
+                        v1.push_back(s);
+                } else {
+                        String semi(";");
+                        v1.push_back(semi);
+                        String temp;
+                        std::cout << "Infix: ";
+                        for (int i=0; i<v1.size(); ++i) {
+                                if(v1.at(i) != ";") {
+                                       std::cout << v1.at(i) << " ";
+                                } else {
+                                        std::cout << v1.at(i);
+                                }
+                        }
+
+                        String rlt = infix_to_postfix(v1);
+                        std::cout << "\nPostfix: " << rlt << "\n"; 
+                        evaluate(rlt, std::cout);
+                        std::cout << "\n";
+
+                        //out << "Prefix: ";
+                        //String prefix = infix_to_prefix(v1);
+                        //out << prefix << "\n\n";
+                        //evaluate_prefix(prefix, out);
+                        //out << "\n";
+
+                         v1.clear();
+                }
 
         }
 	if(argc == 3) {
@@ -79,7 +91,14 @@ while(in>>s) {  if(argc == 2) {
                   	out << "\nPostfix: " << rlt << "\n"; 
 			evaluate(rlt, out);
 			out << "\n";
-                        v1.clear();
+
+			//out << "Prefix: ";
+			//String prefix = infix_to_prefix(v1);
+			//out << prefix << "\n\n";
+			//evaluate_prefix(prefix, out);
+			//out << "\n";
+
+			 v1.clear();
                 }
         }
 }
--- cmontg17/assembler/utilities.hpp
+++ cmontg17/assembler/utilities.hpp
@@ -10,6 +10,10 @@ String evaluate( String s, std::ostream& );
 
 String evaluate(String left, String t, String right, int c, std::ostream&);
 
+String infix_to_prefix(std::vector<String>);
 
+String evaluate_prefix( String s, std::ostream& );
+
+String evaluate_prefix(String left, String t, String right, int c, std::ostream&);
 
 #endif
